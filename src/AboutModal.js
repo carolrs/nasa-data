33e44e3e50e91492917cf4cb9c8f37ea35c3e241
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-06-25 02:33:18 +0100

    about component

--- src/AboutModal.js
+++ src/AboutModal.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React from "react";
+
+const Section = ({ title, children }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <div className="section">
+      <h3 onClick={() => setIsOpen(!isOpen)}>{title}</h3>
+      {isOpen && <div>{children}</div>}
+    </div>
+  );
+};
 
 const AboutModal = ({ onClose }) => (
-    <div className="about">
-        <h2 className="font-bold  text-2xl mb-4 lg:text-4xl">
-            About the App
-        </h2>
-        <div className="star-wars">
-            <p className="crawl">
-                This app is a visual representation of our solar system,
-                showcasing the orbits and rotations of the planets around the Sun.
-                It creates an engaging and interactive experience by animating the planets
-                in their respective orbits. Users can observe the planets' movements
-                and learn about their characteristics and features.
-                The app offers a visually captivating way to explore the solar system
-                and deepen one's understanding of planetary motion.
-                Whether for educational purposes or simply for enjoyment,
-                this app provides an immersive journey through our celestial neighborhood.
-            </p>
-        </div>
+  <div className="about">
+    <br />
+    <div className="star-wars">
+      <Section title="Astronomy Picture of the Day (APOD)">
+        <p className="crawl">
+          Our APOD feature allows you to discover a new celestial wonder every
+          day. Accompanied by detailed explanations, you can learn about our
+          universe in a visually striking way.
+        </p>
+      </Section>
+      <Section title="Near Earth Objects (NEO)">
+        <p className="crawl">
+          The NEO feature provides you with real-time data about objects that
+          have recently passed close to Earth. With information like the close
+          approach date and the estimated diameter, this feature helps you
+          comprehend the activity that occurs in our planet's vicinity.
+        </p>
+      </Section>
+      <Section title="Image Search">
+        <p className="crawl">
+          Powerful search tool taps into NASA's vast image database, enabling
+          you to find stunning visuals of distant galaxies, nebulae, planets,
+          and more. Click on an image to view it in greater detail within a
+          dedicated modal.
+        </p>
+      </Section>
     </div>
+  </div>
 );
 
 export default AboutModal;
